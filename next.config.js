efad35a2cdc38b1bf02f27615d695209c50f06d3
MatiCBL <brzoza.mathew@gmail.com>
2023-07-24 18:52:54 +0100

    'initial-commit'

---
+++ next.config.js
@@ -0,0 +1,22 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    experimental: {
+      appDir: true,
+    },
+    images: {
+      remotePatterns: [
+        {
+          protocol: "https",
+          hostname: "pbs.twimg.com",
+          pathname: "/**",
+        },
+        {
+          protocol: "https",
+          hostname: "i.ytimg.com",
+          pathname: "/**",
+        },
+      ],
+    },
+  };
+  
+  module.exports = nextConfig;
